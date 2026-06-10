5300048a90d3825288cf8d3c158e975957a6bd5e
xx71214xx-hub <xx71214xx@gmail.com>
2026-06-10 23:07:54 +0300

    Update supabaseClient.js

--- PanicArcade/src/api/supabaseClient.js
+++ PanicArcade/src/api/supabaseClient.js
@@ -1,6 +1,6 @@
 // تهيئة عميل Supabase للاتصال بقاعدة البيانات
-const SUPABASE_URL = 'https://cijzcbuhdaimtulsbhak.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNpanpjYnVoZGFpbXR1bHNiaGFrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODEwMzU2NjAsImV4cCI6MjA5NjYxMTY2MH0.qBpXibaiZq-uBMeKnI4rVEnIQ6CrorLlAm25uuTbU_U';
+const SUPABASE_URL = 'https://urxcrkerzxjabqcizymt.supabase.co/rest/v1/';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVyeGNya2VyenhqYWJxY2l6eW10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODExMTQyMTMsImV4cCI6MjA5NjY5MDIxM30.0pPJA7d6FkCYubEjWPGDJF-riZt1TJNT4Ibk2EnsyuE';
 
 // تصدير العميل ليتم استخدامه في باقي ملفات المشروع
 export const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
