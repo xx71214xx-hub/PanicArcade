046f5a74b692f7dd1ba267da2a04abd3ce3b4f91
xx71214xx-hub <xx71214xx@gmail.com>
2026-06-10 23:01:11 +0300

    Delete PanicArcade/config.js

--- PanicArcade/config.js
+++
@@ -1,7 +0,0 @@
-// config.js - إعدادات عامة (لا تضع التوكن السري هنا)
-window.APP_CONFIG = {
-  CHANNEL_USERNAME: "s3_s4",
-  CHANNEL_LINK: "https://t.me/s3_s4",
-  VERIFY_ENDPOINT: "server.php",   // المسار النسبي لملف التحقق
-  XP_DIVISOR: 1000                 // كل 1000 عملة = 1 XP
-};
